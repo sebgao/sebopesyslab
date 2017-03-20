8f37102eedffb5917b36dd3ef387c06e289742ac
Gao <gzt@outlook.com>
2017-03-20 18:12:42 +0800

    Sync some files

---
+++ obj/boot/bootloader.d
@@ -0,0 +1,2 @@
+obj/boot/bootloader.o: boot/bootloader.c /usr/include/stdc-predef.h \
+ inc/x86.h inc/types.h inc/elf.h inc/disk.h
---
+++ obj/boot/mbr.d
@@ -0,0 +1,1 @@
+obj/boot/mbr.o: boot/mbr.S /usr/include/stdc-predef.h inc/mmu.h
